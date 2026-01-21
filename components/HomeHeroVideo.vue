90aa63f6c7d650a299e103ce57e3f1a1e8ed218b
Jean de Villiers <jeandevilliers@me.com>
2026-01-21 15:01:09 +0000

    Create HomeHeroVideo component, adjust layout & styling

---
+++ components/HomeHeroVideo.vue
@@ -0,0 +1,23 @@
+<template>
+	<iframe
+		class="yt-embed"
+		width="560"
+		height="315"
+		src="https://www.youtube.com/embed/pp3B2S-QI0k?si=KEaH0ChcghDZXFN0"
+		title="YouTube video player"
+		frameborder="0"
+		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+		referrerpolicy="strict-origin-when-cross-origin"
+		allowfullscreen
+	></iframe>
+</template>
+
+<style scoped>
+/* Optional: make it responsive */
+:global(.yt-embed) {
+	aspect-ratio: 16 / 9;
+	width: 100%;
+	background: #000;
+	border-radius: 0.5em;
+}
+</style>
