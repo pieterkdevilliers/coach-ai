e375557e4b2cea2206e9e04f3b1b70ab4c980bfe
Jean de Villiers <jeandevilliers@me.com>
2026-01-21 17:39:02 +0000

    Add sign-up form to login page

--- pages/login.vue
+++ pages/login.vue
@@ -6,16 +6,29 @@
 					<LoginForm />
 				</div>
 			</div>
+			<div class="container__inner">
+				<h3 class="text-center text-white mt-12">
+					Don't have an account yet?
+				</h3>
+				<div class="form-container signup">
+					<SignupForm />
+				</div>
+			</div>
 		</div>
 	</section>
 </template>
 
 <script setup lang="ts">
 import { useAuthStore } from '~/stores/auth';
+import SignupForm from '~/components/SignupForm.vue';
 const authStore = useAuthStore();
 
 authStore.clearUniqueAccountId();
 authStore.clearAuthToken();
 </script>
 
-<style scoped></style>
+<style scoped>
+.form-container.signup {
+	@apply mt-3 p-0 shadow-lg;
+}
+</style>
