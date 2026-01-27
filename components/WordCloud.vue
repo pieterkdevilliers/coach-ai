85059585cd1a8997db3167358beabbd568cdffef
Jean de Villiers <jeandevilliers@me.com>
2026-01-27 13:00:25 +0000

    Create & import basic component

---
+++ components/WordCloud.vue
@@ -0,0 +1,11 @@
+<template>
+	<h3>Word Cloud Component</h3>
+	<h4>Account Unique ID: {{ account_unique_id }}</h4>
+</template>
+<script setup lang="ts">
+console.log('Wordcloud component loaded');
+
+const authStore = useAuthStore();
+const account_unique_id = authStore.uniqueAccountId;
+const apiAuthorizationToken = authStore.access_token;
+</script>
